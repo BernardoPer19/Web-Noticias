50f5f1e6de57db2a0f4646d7d317039a9f62a0b1
b.eww_ <148910513+BernardoPer19@users.noreply.github.com>
2025-02-04 10:47:12 -1100

    Update NewsList.tsx

--- src/components/Fetch Components/NewsList.tsx
+++ src/components/Fetch Components/NewsList.tsx
@@ -1,19 +1,1 @@
-// import useFetchNews from "@/hooks/useFetchNews";
 
-//  const NewsComponent = async () => {
-//     const { fetchGeneralNews } = useFetchNews();
-//     const news = await fetchGeneralNews();
-//     return (
-//         <div>
-//             <h2>Últimas Noticias</h2>
-//             {news.map((article) => (
-//                 <div key={article.url}>
-//                     <h3>{article.title}</h3>
-//                     <p>{article.description}</p>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default NewsComponent;
