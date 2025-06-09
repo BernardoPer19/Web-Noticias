8314d65a4076aecd078514a35366b7ead00c33f6
jhoaking <wawxper08@gmail.com>
2025-06-09 04:55:14 -1100

    añadiendo cambios para produccion

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      // 🔴 Desactiva la advertencia del <img>
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
